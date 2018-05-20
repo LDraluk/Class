9b60cdfba78047bf22856ebcee302b3f1fcc8afa
Leah Draluk <leah@draluk.com>
2018-05-20 09:10:05 -0700

    Added knapsack

--- week2/fractional_knapsack.cpp
+++ week2/fractional_knapsack.cpp
@@ -6,14 +6,14 @@ struct item {
     int v;
     int w;
     item(int w, int v) : w(w) , v(v) {};
-
+    
 };
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
     double value = 0.0;
     size_t size = values.size();
     std::vector<item> items;
-   // items.resize(size);
+    // items.resize(size);
     for (int i=0 ; i<size; i++) {
         items.push_back(item(weights[i], values[i]));
     }
@@ -53,3 +53,4 @@ int main() {
     std::cout << optimal_value << std::endl;
     return 0;
 }
+
